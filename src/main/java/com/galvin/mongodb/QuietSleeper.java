166060cb341c970e889d5f5520cbdfbc489ff42b
Thomas Galvin <tgalvin@caci.com>
2015-07-09 14:53:38 -0400

    Allowed Quiet Sleeper to return a collection in one step

--- src/main/java/com/galvin/mongodb/QuietSleeper.java
+++ src/main/java/com/galvin/mongodb/QuietSleeper.java
@@ -8,79 +8,59 @@ public class QuietSleeper
 {
     private static final Logger logger = LoggerFactory.getLogger( QuietSleeper.class );
     
-    /**
-    Sleeps until a MongoDB Collection is available. By default, sleeps for 0, 5000, 10000, 20000, and 30000 milliseconds
-    @param connection a connection to MongoDB
-    @param collectionName the name of the collection you want to retrieve
-    @throws PersistenceException on error, or if the connection never becomes available
-    */
-    public static void sleepUntilAvailable( MongoDbConnection connection, String collectionName  )
+    public static DBCollection sleepUntilAvailable( MongoDbConnection connection, String collectionName  )
         throws PersistenceException
     {
-        sleepUntilAvailable( connection, collectionName, 0, 1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000, 
-                                                            1000 );
+        return sleepUntilAvailable( connection, collectionName, 0, 1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000, 
+                                                                   1000 );
     }
     
-    /**
-    Sleeps until a MongoDB Collection is available.
-    @param connection a connection to MongoDB
-    @param collectionName the name of the collection you want to retrieve
-    @param sleepTimes an unbounded number of sleep times; e.g. 1000, 10000, 20000, 30000 milliseconds
-    @throws PersistenceException on error, or if the connection never becomes available
-    */
-    public static void sleepUntilAvailable( MongoDbConnection connection, String collectionName, int ... sleepTimes )
+    public static DBCollection sleepUntilAvailable( MongoDbConnection connection, String collectionName, int ... sleepTimes )
         throws PersistenceException
     {
-        if( !isConnectionAvailable( connection, collectionName ) )
-        {
-            for( int sleepTime : sleepTimes )
-            {
-                if( isConnectionAvailable( connection, collectionName ) )
-                {
-                    logger.info( "Collection " + collectionName + " is available." );
-                    return;
+        DBCollection result = getConnection( connection, collectionName );
+        if( result == null ) {
+            for( int sleepTime : sleepTimes ) {
+                result = getConnection( connection, collectionName );
+                if( result != null ) {
+                    return result;
                 }
-                else
-                {
-                    logger.info( "Collection " + collectionName + " is not available; sleeping for: " + sleepTime + " milliseconds." );
+                else {
                     quietlySleep( sleepTime );
                 }
             }
             
             throw new PersistenceException( "Collection " + collectionName + " never became available." );
         }
-        else
-        {
-            logger.info( "Collection " + collectionName + " is available." );
-        }
+        return result;
     }
     
     private static void quietlySleep( long sleepTime )
@@ -93,16 +73,12 @@ public class QuietSleeper
         {}
     }
     
-    private static boolean isConnectionAvailable( MongoDbConnection connection, String collectionName )
+    private static DBCollection getConnection( MongoDbConnection connection, String collectionName )
     {
-        try
-        {
-            DBCollection collection = connection.getCollection( collectionName );
-            return collection != null;
-        }
-        catch( Throwable t )
-        {
-            return false;
+        try {
+        return connection.getCollection( collectionName );
+        } catch( Throwable t ) {
+            return null;
         }
     }
 }
